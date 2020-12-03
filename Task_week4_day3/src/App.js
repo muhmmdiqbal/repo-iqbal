c05575933f2c61e9e6ea89d23bb80e8fdca25f8a
muhmmqibal <muhammadiqbal130996@gmail.com>
2020-12-03 09:35:16 +0700

    tugas react

---
+++ Task_week4_day3/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="body">
+      <header><h1 contenteditable>Header.com</h1></header>
+    <div class="left-sidebar" contenteditable>Left Sidebar</div>
+    <main contenteditable></main>
+    <div class="right-sidebar" contenteditable>Right Sidebar</div>
+    <footer contenteditable>Footer Content — Header.com 2020</footer> 
+    </div>
+  );
+}
+
+export default App;
